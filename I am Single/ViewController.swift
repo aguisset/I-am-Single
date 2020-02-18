afa28ab9a25c7b9adb157d073852df9d1d946a11
Abdoul Guisset <guissetabdoul@gmail.com>
2020-02-18 15:05:51 -0500

    First iOS project

---
+++ I am Single/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am Single
+//
+//  Created by Abdoul Guisset on 2/18/20.
+//  Copyright © 2020 Abdoul Guisset. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
